641834255818991ee48eadc888918465433457d1
PatrykTru <patryk.truchan94@gmail.com>
2022-02-13 14:49:03 +0100

    6.2 Added Calculator class and 2 unit tests.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("ForumUser");
+        Calculator calculator = new Calculator();
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("ForumUser"))
+            System.out.println("test OK");
+        else
+            System.out.println("Error");
+
+        if(calculator.add(2,2) == 4)
+            System.out.println("test2 OK");
+        else
+            System.out.println("Error");
+
+        if(calculator.substract(2,2) == 0)
+            System.out.println("test3 OK");
+        else
+            System.out.println("Error");
+
+
 
     }
 }
