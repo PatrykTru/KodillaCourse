ce93e911dd80261b75853e30184f66d3a5c5a471
PatrykTru <patryk.truchan94@gmail.com>
2022-02-16 20:07:40 +0100

    6.5 Adding WeatherForecast project.

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.weather.stub;
+
+import java.util.*;
+
+public class WeatherForecast {
+    private Temperatures temperatures;
+
+    public WeatherForecast(Temperatures temperatures) {
+        this.temperatures = temperatures;
+    }
+
+    public Map<String,Double> calculateForecast(){
+        Map<String, Double> resultMap = new HashMap<>();
+
+        for (Map.Entry<String,Double> temperature: temperatures.getTemperatures().entrySet()){
+            resultMap.put(temperature.getKey(),temperature.getValue());
+        }
+        return resultMap;
+    }
+
+    public Double calculateForecastAverage(){
+        Double sum= 0.0;
+        int count=0;
+
+        for (Map.Entry<String,Double> temperature: temperatures.getTemperatures().entrySet()){
+            sum =sum+ temperature.getValue();
+            count++;
+        }
+        return sum/count;
+    }
+
+    public Double calculateForecastMediana(){
+        Double mediana = 0.0;
+        List<Double> temperaturesList = new ArrayList<>();
+
+        for (Map.Entry<String,Double> temperature: temperatures.getTemperatures().entrySet()){
+            temperaturesList.add(temperature.getValue());
+        }
+        Collections.sort(temperaturesList);
+
+        if(temperaturesList.size()%2==0)
+            mediana = temperaturesList.get((temperaturesList.size()-1)/2+1);
+        else
+            mediana =  temperaturesList.get((temperaturesList.size()-1)/2);
+
+        return mediana;
+
+    }
+
+
+
+
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -0,0 +1,72 @@
+package com.kodilla.testing.weather.mock;
+
+import com.kodilla.testing.weather.stub.Temperatures;
+import com.kodilla.testing.weather.stub.WeatherForecast;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class WeatherForecastTestSuite {
+
+    @Mock
+    private Temperatures temperaturesMock ;
+    Map<String, Double> temperaturesMap = new HashMap<>();
+    
+    @BeforeEach
+    void setUp() {
+        temperaturesMap.put("Wroclaw", 24.8);
+        temperaturesMap.put("Warszawa", 25.2);
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Gdansk", 26.1);
+        temperaturesMap.put("Krakow", 26.2);
+
+
+
+    }
+
+    @Test
+    void calculateWeatherForecastWithMock() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+        //Then
+        Assertions.assertEquals(5,quantityOfSensors);
+    }
+
+    @Test
+    void calculateAverageWithMock() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        //When
+        Double average = weatherForecast.calculateForecastAverage();
+        //Then
+        Assertions.assertEquals(25.56,average);
+
+
+    }
+    @Test
+    void calculateMedianaWithMock() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        //When
+        Double average = weatherForecast.calculateForecastMediana();
+        //Then
+        Assertions.assertEquals(25.5,average);
+
+
+    }
+}
