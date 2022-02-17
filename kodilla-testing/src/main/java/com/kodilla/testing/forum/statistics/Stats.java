9619da1b1ee02b38e077cc1c6fa0bce8c4800d6f
PatrykTru <patryk.truchan94@gmail.com>
2022-02-17 21:39:46 +0100

    6.7 Adding Statistics to Forum project.

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/Stats.java
@@ -0,0 +1,43 @@
+package com.kodilla.testing.forum.statistics;
+
+public class Stats {
+
+    int usersCount,postCount,commentsCount;
+    double averagePostsPerUser,averageCommentsPerUser, averageCommentsPerPost;
+
+
+
+    void calculateAdvStatistics(Statistics statistics){
+        usersCount = statistics.usersNames().size();
+        postCount = statistics.postsCount();
+        commentsCount = statistics.commentsCount();
+
+        if(postCount>0 && usersCount>0){
+            averagePostsPerUser = statistics.postsCount()/usersCount;}
+        else {
+            averagePostsPerUser  = 0;}
+
+        if(commentsCount>0 && usersCount>0){
+            averageCommentsPerUser = statistics.commentsCount()/usersCount;}
+        else {
+            averageCommentsPerUser = 0;
+        }
+
+        if(postCount>0 && commentsCount>0){
+            averageCommentsPerPost = statistics.commentsCount()/statistics.postsCount();}
+        else {
+            averageCommentsPerPost = 0;
+        }
+    }
+
+    String showStatistics(){
+
+
+        return "UsersCount: " + usersCount
+                + "PostCount: " + postCount
+                + "CommentsCount: " + commentsCount
+                + "Average posts per user: " + averagePostsPerUser
+                + "Average comments per user: " + averageCommentsPerUser
+                + "Average comments per post: " + averageCommentsPerPost;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -0,0 +1,145 @@
+package com.kodilla.testing.forum.statistics;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class StatisticsTestSuite {
+
+    @Mock
+    Statistics statisticsMock;
+
+    private List<String> generateListOfUsers(int usersQuantity) {
+        List<String> resultList = new ArrayList<>();
+        for (int n = 1; n <= usersQuantity; n++) {
+            String theUser = "User " + n;
+            resultList.add(theUser);
+        }
+        return resultList;
+    }
+
+    @Test
+    void calculateAdvStatisticsWhen0Posts() {
+       //Given
+        Stats stats = new Stats();
+        stats.postCount= 0;
+        when(statisticsMock.postsCount())
+                .thenReturn(stats.postCount);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertEquals(0,statisticsMock.postsCount());
+
+
+    }
+
+    @Test
+    void calculateAdvStatisticsWhen1000Posts() {
+        //Given
+        Stats stats = new Stats();
+        stats.postCount= 1000;
+        when(statisticsMock.postsCount())
+                .thenReturn(stats.postCount);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+
+        assertEquals(1000,statisticsMock.postsCount());
+
+    }
+
+    @Test
+    void calculateAdvStatisticsWhen0Comments() {
+        //Given
+        Stats stats = new Stats();
+        stats.commentsCount = 0;
+        when(statisticsMock.commentsCount())
+                .thenReturn(stats.commentsCount);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertEquals(0, statisticsMock.commentsCount());
+
+    }
+    @Test
+    void calculateAdvStatisticsWhenCommentsLessThanPosts() {
+
+        //Given
+        Stats stats = new Stats();
+        stats.commentsCount = 5;
+        stats.postCount = 10;
+        when(statisticsMock.commentsCount())
+                .thenReturn(stats.commentsCount);
+        when(statisticsMock.postsCount()).thenReturn(stats.postCount);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(statisticsMock.commentsCount() < statisticsMock.postsCount());
+    }
+    @Test
+    void calculateAdvStatisticsWhenPostsLessThanComments() {
+
+        //Given
+        Stats stats = new Stats();
+        stats.commentsCount = 15;
+        stats.postCount = 10;
+        when(statisticsMock.commentsCount())
+                .thenReturn(stats.commentsCount);
+        when(statisticsMock.postsCount()).thenReturn(stats.postCount);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertTrue(statisticsMock.commentsCount() > statisticsMock.postsCount());
+
+    }
+    @Test
+    void calculateAdvStatisticsWhen0Users() {
+
+        //Given
+        Stats stats = new Stats();
+        List<String> testUsers = new ArrayList<>();
+
+
+        when(statisticsMock.usersNames())
+                .thenReturn(testUsers);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertEquals(0,statisticsMock.usersNames().size());
+
+    }
+    @Test
+    void calculateAdvStatisticsWhen100Users() {
+
+        //Given
+        Stats stats = new Stats();
+        List<String> testUsers = generateListOfUsers(100);
+
+        when(statisticsMock.usersNames())
+                .thenReturn(testUsers);
+        //When
+        stats.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        assertEquals(100,statisticsMock.usersNames().size());
+
+
+    }
+}
